aa581eff495643986081c2c88fcda6e2a12ce5f7
Gentrit Biba <gentritbiba1@gmail.com>
2020-05-12 00:34:23 +0200

    Frist push

---
+++ ajax.php
@@ -0,0 +1,92 @@
+<?php
+$path = preg_replace('/wp-content.*$/','',__DIR__);
+include_once($path.'wp-load.php');
+
+if ( isset($_POST['action']) && $_POST['action'] == 'link_click_counter' && isset( $_POST['nonce'] ) &&  isset( $_POST['post_id'] ) && wp_verify_nonce( $_POST['nonce'], 'link_click_counter_' . $_POST['post_id'] ) ) {
+    $count = get_post_meta( $_POST['real_id'], 'link_click_counter', true );
+    update_post_meta( $_POST['real_id'], 'link_click_counter', ( $count === '' ? 1 : $count + 1 ) );
+}
+if ( isset($_POST['action']) && $_POST['action'] == 'bs_gen_mass_url' && isset( $_POST['nonce'] ) &&  isset( $_POST['post_type'] ) && wp_verify_nonce( $_POST['nonce'], 'gen_mass_url' ) ) {
+    
+    $args= array(
+        'post_type' =>$_POST['post_type'],
+        'meta_query' => array(
+        array(
+         'key' => 'shorten_url',
+         'compare' => 'NOT EXISTS' // this should work...
+        ),
+    ));
+    $posts = get_posts($args);
+    $counter=0;
+    foreach($posts as $post){
+        $api_key = get_option( 'shorten_api_key' );
+        $domain = get_option( 'shorten_domain' );
+        $generated_url = generate_shorten_url($api_key, $domain, get_permalink($post_id));
+        $up = false;
+        if(!$generated_url->error){
+            $up = update_post_meta($post->ID, 'shorten_url', $generated_url->fullUrl);
+        }
+        else{
+            $err = $generated_url->error;
+        }
+            // $up = delete_post_meta($post->ID, 'shorten_url');
+        if($up){
+            $counter++;
+        }
+    }
+    $total_posts = count($posts);
+    if($counter == $total_posts){
+        echo json_encode(array(
+            'success'   => true,
+            'updated'   => $counter
+        ));
+    }
+    else{
+        echo json_encode(array(
+            'failed'    => true,
+            'total'     => $total_posts,
+            'updated'   => $counter,
+            'error'   => $err,
+        ));
+    }
+
+
+}
+
+
+add_action( 'wp_head', 'link_click_head' );
+function link_click_head() {
+    global $post;
+
+    if( isset( $post->ID ) ) {
+?>
+    <script type="text/javascript" >
+    jQuery(function ($) {
+        $( '.share-social-buttons-wrapper a' ).on( 'click', function() {
+            event.preventDefault();
+            var self = $( this );
+            var cc_ajax_options = {
+                action: 'link_click_counter',
+                nonce: '<?php echo wp_create_nonce( 'link_click_counter_' . $post->ID ); ?>',
+                ajaxurl: '<?php echo SHORTEN_PLUGIN_URL . 'ajax.php' ?>',
+                post_id: '<?php echo $post->ID; ?>',
+                real_id: self.data('post-id')
+            };
+            if(!localStorage.getItem('bs_prevent_clicked_share')){
+                localStorage.setItem('bs_prevent_clicked_share',true);
+                setTimeout(function() {
+                    localStorage.removeItem('bs_prevent_clicked_share');
+                }, 2000);
+            $.post( cc_ajax_options.ajaxurl, cc_ajax_options, function() {
+                window.open(self.attr('href'), 'mywindow', 'location=no,status=1,scrollbars=1,width=500,height=650');
+            });
+            }
+            return false;
+        });
+
+    });
+    </script>
+<?php
+    }
+}
+?>
---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+(function( $ ) {
+    $(function() {
+        jQuery('input[type=radio][name=shorten_icon_color]').change(function() {
+            if (this.value == 'custom') {
+                jQuery(".shorten_icon_color_custom_text").show();
+                jQuery('.wp-picker-container').show();
+            }
+            else{
+                jQuery(".shorten_icon_color_custom_text").hide();
+                jQuery('.wp-picker-container').hide();
+
+            }
+        });
+        jQuery('input[type=radio][name=shorten_icon_size]').change(function() {
+            if (this.value == 'custom') {
+                jQuery(".shorten_icon_size_custom_label").show();
+            }
+            else{
+                jQuery(".shorten_icon_size_custom_label").hide();
+
+            }
+        });
+        jQuery('#shorten_url_box_enabled').change(function() {
+            jQuery('#shorten_url_box_enabled:checked').length
+            ?jQuery('.url-box-border-options').css('display','block')
+            :jQuery('.url-box-border-options').css('display','none')
+        });
+        
+
+        // Add Color Picker to all inputs that have 'color-field' class
+        let button_color_input = $( '.shorten_icon_color_custom' );
+        let border_color_input = $( '.shorten_url_box_border_color_custom' );
+        $( '#shorten_icon_color_custom_text' ).wpColorPicker();
+        $( '#shorten_url_box_border_color_custom_text' ).wpColorPicker({defaultColor : "#ffffff"});
+        if($('#shorten_icon_color_custom_text').length){
+            function rgb2hex(rgb){
+                // if(!rgb)return;
+                // rgb = rgb.match(/^rgb((d+),s*(d+),s*(d+))$/);
+                rgb = rgb.replace("rgb(","");
+                rgb = rgb.replace(")","");
+                rgb = rgb.replace(" ","");
+                rgb = rgb.replace(" ","");
+
+                rgb = rgb.split(',');
+
+                return "#" +
+                 ("0" + parseInt(rgb[0],10).toString(16)).slice(-2) +
+                 ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+                 ("0" + parseInt(rgb[2],10).toString(16)).slice(-2);
+               }
+            (function() {
+                var ev = new $.Event('style'),
+                    orig = $.fn.css;
+                $.fn.css = function() {
+                    $(this).trigger(ev);
+                    return orig.apply(this, arguments);
+                }
+            })();
+            $('.button-color .wp-color-result').css('background-color',$('#shorten_icon_color_custom_text').val());
+
+            $('.button-color .wp-color-result').bind('style', function(e){
+                let hex = rgb2hex(this.style['background-color']);
+                // console.log(rgb2hex(this.style['background-color']));
+                button_color_input.val(hex);
+                button_color_input[0].defaultValue = hex;
+                
+                
+            });
+            $('.url-box-border .wp-color-result').bind('style', function(e){
+                let hex = rgb2hex(this.style['background-color']);
+                // console.log(rgb2hex(this.style['background-color']));
+                border_color_input.val(hex);
+                border_color_input[0].defaultValue = hex;
+                
+                
+            });
+            
+
+        }
+         
+        if(jQuery('input[type=radio][name=shorten_icon_color]:checked').val()!= "custom"){
+            jQuery('.wp-picker-container').hide();
+        } 
+        $('.url-box-border .wp-color-result').css("background-color",$( '#shorten_url_box_border_color_custom_text' ).val());
+
+    });
+    
+})( jQuery );
